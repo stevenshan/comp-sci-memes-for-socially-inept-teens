f8ab697eae9a04ad739e750dddf229726853de4b
Steven Shan <steven.shan99@gmail.com>
2018-06-24 18:51:08 -0400

    infinite scrolling front page gallery

--- general.js
+++ general.js
@@ -1,57 +1,206 @@
 // parameters, can change these
-var imageLoadNum = 20; // number of images to load at a time
+var imageLoadNum = 1; // number of images to load at a time
+// distance from image to each side of block
+var imagePadding = 20;
 
 // global variables, no need to manually change
 var imagesDir = "images/"
 var images = [];
 var index = 0;
+var loading = false;
 
-function loadImages(count)
+function getColumnHeight(column)
 {
-    var allImages = "";
-    for (var i = 0; i < count && index < images.length; i++)
+    var attr = column.imageHeight;
+    if (attr == undefined)
     {
+        return 0;
+    }
+    else
+    {
+        return parseInt(column.imageHeight);
+    }
+}
+
+function addColumnHeight(column, value)
+{
+    column.imageHeight = getColumnHeight(column) + value;
+}
+
+// get column of shortest height
+function getMinColumn()
+{
+    // find shortest column to add image to
+    var minHeight = -1;
+    var column = undefined;
+    var columns = $("#photos .column")
+    var i = columns.length - 1;
+    while (i >= 0)
+    {
+        var curColumn = columns.get(i); 
+        var colHeight = getColumnHeight(curColumn);
+
+        if (colHeight <= minHeight || minHeight == -1)
+        {
+            minHeight = colHeight;
+            column = $(curColumn);
+        }
+
+        i -= 1;
+    }
+
+    return {"column": column, "height": minHeight};
+}
+
+function loadImages(count, callback = undefined)
+{
+    if (loading)
+    {
+        return false;
+    }
+
+    loading = true;
+    var origIndex = index;
+
+    // loop to add images to page
+    var k = 0;
+    var colWidth = $("#photos .column").width() - 5;
+    var next = function(k) {
+        // loop end condition
+        if (k >= count || origIndex >= images.length)
+        {
+            loading = false;
+            if (typeof callback === "function")
+            {
+                callback();
+            }
+            return;
+        }
+
         // get next available image
-        image = images[index];
-        index += 1;
+        var image = images[origIndex];
+        origIndex += 1;
 
         // get path to image
-        path = imagesDir + image["filePath"];
+        var path = imagesDir + image["_filePath"];
 
-        allImages += 
-            '<div class="block">' + 
+        // find shortest column to add image to
+        var column = getMinColumn()["column"];
+
+        // generate html for adding image to page
+        var id = "image" + origIndex;
+        html = $(
+            '<div class="block" id="' + id + '">' + 
                 '<div class="imgWrapper">' + 
                     '<img src="' + path + '">' +
                 '</div>' +
-            '</div>';
+            '</div>');
+
+        // add image to shortest column
+        column.append(html);
+
+        // get height of image just added
+        var ratio = 1.0 * image["_imageSize"]["y"] / image["_imageSize"]["x"];
+        // subtract imagePadding to get max width of image
+        var newHeight = ratio * (colWidth - imagePadding * 2);
+
+        addColumnHeight(column.get(0), newHeight + imagePadding * 2);
+
+        next(k + 1);
     }
 
-    $('#photos').append(allImages);
+    // add images once they've been preloaded
+    var queuedAllPreload = false;
+    var preloadQueueSize = 0;
+    function preloadDone()
+    {
+        preloadQueueSize -= 1;
+
+        // update image properties
+        var obj = images[this.index];
+        obj["_imageSize"]["x"] = this.width;        
+        obj["_imageSize"]["y"] = this.height;        
+
+        // done preloading
+        if (preloadQueueSize == 0 && queuedAllPreload)
+        {
+            // start loop
+            next(0);
+        }
+    }
+
+    // preload images
+    for (var i = 0; i < count && index < images.length; i++)
+    {
+        // get next available image
+        var image = images[index];
+        index += 1;
+
+        // get path to image
+        var path = imagesDir + image["_filePath"];
+
+        // preload individual image
+        temp = new Image();
+        temp.src = path;
+        temp.index = index - 1;
+        temp.onload = preloadDone;
+
+        preloadQueueSize += 1;
+    }
+
+    queuedAllPreload = true;
+
 }
 
 // event to check scroll position to see if page is at bottom
 // so more images need to be loaded
-function scrollEvent()
+// returns whether more images needed to be loaded
+function scrollEvent(callback = undefined)
 {
     // make sure there are more images to load
-    if (index >= images.length) return;
-    
-    // current scroll position in window
-    var scrollPosition = $(window).scrollTop(); 
-
-    // total height of entire document
-    var docHeight = $(document).height();
-    var windowHeight = $(window).height();
-
-    // check if scrolled to page bottom
-    if (scrollPosition + windowHeight + 50 >= docHeight)
+    if (index >= images.length)
     {
-        loadImages(imageLoadNum);
+        if (typeof callback === "function") 
+            callback(false);
+        return false;
+    } 
+
+    // don't try to load more until finished loading previous images
+    if (loading == true)
+    {
+        return false;
     }
+
+    var position = window.innerHeight + window.scrollY;
+    var docHeight = document.body.offsetHeight;
+
+    // get minimum height of columns
+    var temp = getMinColumn();
+    var minHeight = temp["height"];
+    var photosHeight = $("#photos").offset().top + minHeight;
+
+    // check if scrolled to bottom of photos
+    if (position >= photosHeight || position >= docHeight - 10)
+    {
+        var tempCallback = function() { 
+            if (typeof callback === "function") 
+            {
+                callback(true) 
+            }
+            $(window).trigger("scroll");
+        };
+        loadImages(imageLoadNum, tempCallback);
+        return true;
+    }
+
+    if (typeof callback === "function") 
+    {
+        callback(false);
+    }
+    return false;
 }
 
 $(function(){
-
     // load index file to get image urls
     $.getJSON("index.json", function(json){
         imagesDir = json["directory"];
@@ -61,24 +210,30 @@ $(function(){
         for (var url in tempImages)
         {
             image = tempImages[url];
-            image["timestamp"] = Date.parse(image["timestamp"]);
-            image["filePath"] = url;
+            image["_timestamp"] = Date.parse(image["timestamp"]);
+            image["_imageSize"] = {"x": 100, "y": 100};
+            image["_filePath"] = url;
 
             images.push(image);
         }
 
         // sort images by newest to oldest
         images.sort(function(a, b){
-            return b["timestamp"] - a["timestamp"];
+            return b["_timestamp"] - a["_timestamp"];
         });
 
-        // load the first 10 pictures
-        loadImages(imageLoadNum);
+        // load images on page load until page is filled
+        var scrollLoadCallback = function(loadedMore)
+        {
+            if (loadedMore)
+            {
+                scrollEvent(scrollLoadCallback);
+            }
+        }
+        scrollEvent(scrollLoadCallback);
 
         // trigger for scroll event
         $(window).scroll(scrollEvent);
-        // manually trigger scroll at beginning to see if page is filled
-        scrollEvent();
 
     });
 
